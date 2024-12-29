cf9843952a5a759e94946e6507fc41175b9a744f
”koshel99” <yasmina.koshel@live.com>
2024-12-30 01:07:03 +0300

    end

--- models/dog.js
+++ models/dog.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 
 const dogSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    image: String,
+    breed: { type: String, required: true },
+    age: { type: Number, required: true },
+    isAdopted: { type: Boolean, default: false }
 })
 
 const Dog = mongoose.model('Dog', dogSchema)
--- server.js
+++ server.js
@@ -27,12 +27,59 @@ app.get('/dogs/new', (req, res) => {
     res.render('dogs/new.ejs');
 });
 
+// show specific dog
 app.get('/dogs/:dogId', async (req, res) => {
     const dogId = req.params.dogId;
     const dog = await Dog.findById(dogId);
     res.render('dogs/show.ejs', { dog: dog });
 });
 
+// update dog info
+  
+app.put('/dogs/:dogId', async (req, res) => {
+    if (req.body.isAdopted === 'on') {
+      req.body.isAdopted = true;
+    } else {
+      req.body.isAdopted = false;
+    }
+  
+    const dogId = req.params.dogId;
+    await Dog.findByIdAndUpdate(dogId, req.body);
+    res.redirect(`/dogs/${dogId}`);
+});
+
+// edit a specific dog's page
+app.get('/dogs/:dogId/edit', async (req, res) => {
+    const dogId = req.params.dogId;
+    const dog = await Dog.findById(dogId);
+    res.render('dogs/edit.ejs', { dog: dog });
+});
+  
+
+// delete a dog
+app.delete('/dogs/:dogId', async (req, res) => {
+    const dogId = req.params.dogId;
+    await Dog.findByIdAndDelete(dogId);
+    res.redirect('/dogs');
+});
+  
+// list all dogs
+app.get('/dogs', async (req, res) => {
+    const allDogs = await Dog.find();
+    res.render('dogs/index.ejs', { dogs: allDogs });
+});
+  
+// add a new dog
+app.post('/dogs', async (req, res) => {
+    if (req.body.isAdopted === 'on') {
+        req.body.isAdopted = true;
+    } else {
+        req.body.isAdopted = false;
+    }
+    await Dog.create(req.body);
+    res.redirect('/dogs');
+});
+  
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
